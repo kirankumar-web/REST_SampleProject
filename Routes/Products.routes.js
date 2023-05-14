53689f119aad1b189292e9a14a857a8b71ff96d5
kirankumar-web <kirankumaryadav84@gmail.com>
2023-05-14 18:44:01 +0530

    commit

--- Routes/Products.routes.js
+++ Routes/Products.routes.js
@@ -4,7 +4,7 @@ const createError=require('http-errors');
 const mongoose = require('mongoose');
 
 const Product=require('../Models/Product.model');
-const { cast } = require('sequelize');
+const { cast, ValidationError } = require('sequelize');
 //async & await to get the all the list of product from database
 route.get('/',async(req,res,next)=>{
     //res.send('getting all the list of the products')
@@ -23,7 +23,13 @@ route.post('/', async(req, res, next) => {
       const result=await product.save();
       res.send(result);
     } catch (error) {
-        console.log(error.message);
+      console.log(error.message);
+      if (error.name === 'ValidationError') 
+      {
+         next(createError(422,error.message));
+         return;
+      }
+        next(error);
     }
 });
 // promise way (use save option)
@@ -83,31 +89,47 @@ route.get('/:id',async(req,res,next)=>{
    }
 })
 // updating specific product by using its id
-route.put('/:id',async(req,res,next)=>{
-const id=req.params.id;
-try {
-const option={new :true};
-const update=req.body;
-const result=await 
-Product.findByIdAndUpdate(id,update,option);
-res.send(result);
-} 
-catch (error) {
- console.log(error.message);
-}
-    //res.send('updating the specific product')
-})
+route.put('/:id', async (req, res, next) => {
+   const id = req.params.id;
+   try {
+     const option = { new: true };
+     const update = req.body;
+     const result = await Product.findByIdAndUpdate(id, update, option);
+     if (!result) {
+       throw createError(404, "Product doesn't exist");
+     }
+     res.send(result);
+   } catch (error) {
+     if (error instanceof mongoose.CastError) {
+       next(createError(400, "Invalid product ID"));
+       return;
+     }
+     // console.log(error.message);
+     next(error);
+   }
+ });
+ // updating specific product specific data by using its id 
 route.patch('/:id',async(req,res,next)=>{
-   // res.send('updating specific info of the product')
    const id=req.params.id;
    try {
     const update=req.body;
     const option={next: true};
-    const result=await
-     Product.findByIdAndUpdate(id,update,option);
+    const result=await Product.findByIdAndUpdate(id,update,option);
+    if (!result)
+     {
+      throw createError(404,"product doesn't Exist")
+    }
     res.send(result);
-   } catch (error) {
-    console.log(error.message);
+    console.log(result);
+   } catch (error) 
+   {
+    if (error instanceof mongoose.CastError) 
+    {
+      next(createError(400,"invalid Product ID"))
+      return     
+   }
+    //console.log(error.message);
+    next(error);
    }
 })
 route.delete('/:id',async(req,res,next)=>{
