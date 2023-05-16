a4e5410e03f2cef051c349451ddcc7589722a24a
kirankumar-web <kirankumaryadav84@gmail.com>
2023-05-16 06:15:21 +0530

    commiit

--- initDB.js
+++ initDB.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 module.exports=()=>
 {
-    mongoose.connect(process.env.MONGODB_URI)
+   mongoose.connect(process.env.MONGODB_URI)
 .then(()=>
 {
     console.log('mongodb connected....');
